c6e9ce8f87a663ed9b0142d330a9c98972b9141c
vigimus <160164175+vigimus@users.noreply.github.com>
2024-03-21 19:59:45 +0100

    Add files via upload

---
+++ javaScript_Borjan.js
@@ -0,0 +1,142 @@
+JAVASCRIPT TYPER
+-----------------
+1. Nummer
+2. Strängar
+3. Boolean
+4. Undefined
+5. Null
+<!-- 6. Symbol (new in ECMAScript 6) -->
+7. Objekt
+
+1. Nummer
+5 + "34" (Det som kommer hända är att Javascript försöker hjälpa oss
+lösa det här konflikten/felet anta att vi försöker addera två strängar med
+varandra därmed göra om så svaret blir "534") "534"
+
+
+5 - "4" (Liknar situationen som innan dock i det här fallet så kan man inte 
+subtraera i javaScript därmed så kommer terminalen försöka hjälpa oss och göra om båda typerna till nummer eller number så det blir 5 - 4 = 1) 5 - 4 = 1
+
+10 % 5 (Renodlad matte här kommer bli svar 0) 0
+5 % 10 (Renodlad matte här kommer bli svar 5) 5
+
+2. Strängar
+"Java" + "Script" (Eftersom det inte finns några mellanrum här är svaret JavaScript) JavaScript
+
+" " + " " (Då det är två stränger med mellanslag kommer svara bara bli två tomma mellanslag "  ")"  "
+" " + 0
+
+3. Boolean
+true + true (Har man sanning två gånger kan det bara bli sant så svaret är 2) 2
+true + false (Blandar man något som är sant och falskt blir det såklart ändå falskt så svar 1) 1
+false + true (Samma som innan då resultat blir detsamma när man lägger ihop det 1) 1
+false - true (Liknar det som var innan men nu blir det -1) -1
+3 - 4 (Kommer bli -1) -1
+"Bob" - "bill" (Går inte att göra minus på två stänger så felet blir NaN eller resultatet beroende på hur man ser det)NaN står för "not a number"
+
+JAVASCRIPT COMPARISONS
+-----------------
+
+!==
+===
+>=
+<=
+>
+<
+
+JAVASCRIPT VARIABLES
+-----------------
+var
+<!-- let (new in ECMAScript 6)-->  
+<!-- const (new in ECMAScript 6)-->
+
+JAVASCRIPT CONDITIONALS
+-----------------
+if
+else
+else if
+<!-- ternary operator -->
+<!-- switch -->
+
+
+JAVASCRIPT LOGICAL OPERATORS
+-----------------
+&&
+||
+!
+
+JAVASCRIPT FUNCTIONS
+-----------------
+var a = function name() {}
+function name() {}
+return
+<!-- () => (new in ECMAScript 6) -->
+
+JAVASCRIPT DATA STRUCTURES
+-----------------
+Array
+Object
+
+JAVASCRIPT LOOPING
+-----------------
+for
+while
+do 
+forEach (new in ECMAScript 5) 
+
+
+JAVASCRIPT KEYWORDS
+-----------------
+break
+case
+catch
+class
+const
+continue
+debugger
+default
+delete
+do
+else
+export
+extends
+finally
+for
+function
+if
+import
+in
+instanceof
+new
+return
+super
+switch
+this
+throw
+try
+typeof
+var
+void
+while
+with
+yield
+
+
+//Evaluate the below:
+
+
+
+//Evaluate the below comparisons:
+5 >= 1
+0 === 1
+4 <= 1
+1 != 1
+"A" > "B"
+"B" < "C"
+"a" > "A"
+"b" < "A"
+true === false
+true != true
+
+
+// Make the string: "Hi There! It's "sunny" out" by using the + sign:
